e589fedd3d0c8d3d3c7b47540790757612bcb162
CJosephW <wisecameron1@gmail.com>
2019-09-09 14:03:37 -0400

    search_feature finished
    Todo: work on sorting and processing reviews

--- scraper_testing.py
+++ scraper_testing.py
@@ -13,8 +13,11 @@ driver = webdriver.Firefox()
 def search(search_keyword):
     
     driver.get('https://www.amazon.com/s?k='+ search_keyword + '&ref=nb_sb_noss_1')
+    
     time.sleep(5)
+    
     search_index = 1
+    
     results = driver.find_elements_by_css_selector("span.a-size-base-plus.a-color-base.a-text-normal")
 
     for result in results:
@@ -23,16 +26,20 @@ def search(search_keyword):
 
     search_choice = input('please select search result by the number provided')
     search_choice = int(search_choice)
-    results[search_choice].click()
+    results[search_choice + 1].click()
+    
     time.sleep(3)
-    product_details = driver.find_elements_by_tag_name('li')
-    for item in product_details:
-        if len(item.text) == 10:
-            asin = item.text
-                
-    print(asin)
+    
+    url = str(driver.current_url)
+    
+    asin_index = url.index('dp')
+    asin_index += 3
+    
+    asin = url[asin_index: asin_index + 10]
+    
+    return asin
 
-def parse_reviews():
+def parse_reviews(asin):
     
     reviews = []
     review_elements = []
@@ -41,7 +48,7 @@ def parse_reviews():
         
         page_num = str(i + 1)
         
-        driver.get('https://www.amazon.com/product-reviews/B07TZKFJBX/ref=cm_cr_getr_d_show_all?ie=UTF8&reviewerType=all_reviews&pageNumber='+(page_num)+'&sortBy=recent')#gets each page and sorts by most recent
+        driver.get('https://www.amazon.com/product-reviews/'+ asin +'/ref=cm_cr_getr_d_show_all?ie=UTF8&reviewerType=all_reviews&pageNumber='+(page_num)+'&sortBy=recent')#gets each page and sorts by most recent
         review_elements = driver.find_elements_by_xpath("//*[@class = 'a-size-base review-text review-text-content']/span")
         
         reviews += [review.text for review in review_elements]#puts each review in a list
@@ -50,8 +57,9 @@ def parse_reviews():
 
     for review in reviews:#iterates through each item in list
         
-        print(review + 'END OF REVIEW')
+        print(review + 'END OF REVIEW\n')
 
 search_input = input('please enter what you would like to search: ')
-search(search_input)
+
+parse_reviews(search(search_input))
 
